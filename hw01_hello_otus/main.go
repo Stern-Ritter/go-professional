a69438c3d120b5a8682fe69d13ce04df26dbd8f4
Stern_Ritter <fadeef.k@yandex.ru>
2024-06-01 13:10:16 +0300

    feat: add print reversed string func

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	PrintReversedString("Hello, OTUS!")
+}
+
+func PrintReversedString(s string) {
+	fmt.Println(reverse.String(s))
 }
