7cc82585c5cc4df1b36a07ee358d5b3f3eccd513
Stern_Ritter <fadeef.k@yandex.ru>
2024-07-29 14:43:15 +0300

    feat: add file copying tool

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +14,47 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func Copy(fromPath string, toPath string, offset int64, limit int64) error {
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("open file %s: %w", fromPath, ErrUnsupportedFile)
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o755)
+	if err != nil {
+		return fmt.Errorf("open file %s: %w", toPath, ErrUnsupportedFile)
+	}
+	defer toFile.Close()
+
+	fromFileInfo, err := fromFile.Stat()
+	if err != nil {
+		return fmt.Errorf("get stat file %s: %w", fromPath, ErrUnsupportedFile)
+	}
+
+	fromFileSize := fromFileInfo.Size()
+	if offset > fromFileSize {
+		return fmt.Errorf("file %s: %w", fromPath, ErrOffsetExceedsFileSize)
+	}
+
+	_, err = fromFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("seek file %s: %w", fromPath, err)
+	}
+
+	if limit == 0 || limit > fromFileSize-offset {
+		limit = fromFileSize - offset
+	}
+
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+
+	progressReader := bar.NewProxyReader(io.LimitReader(fromFile, limit))
+
+	_, err = io.Copy(toFile, progressReader)
+	if err != nil {
+		return fmt.Errorf("copy file %s to file %s: %w", fromPath, toPath, err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,96 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	testDir := "./testdata"
+	outputDir := filepath.Join(testDir, "output")
+	expectedDir := filepath.Join(testDir, "expected")
+
+	err := os.MkdirAll(outputDir, 0o755)
+	require.NoError(t, err, "unexpected error creating test data output directory")
+	defer os.RemoveAll(outputDir)
+
+	tests := []struct {
+		name          string
+		fromPath      string
+		toPath        string
+		expectedPath  string
+		offset        int64
+		limit         int64
+		expectedError error
+	}{
+		{
+			name:          "file copy full",
+			fromPath:      filepath.Join(testDir, "source.txt"),
+			toPath:        filepath.Join(outputDir, "full_copy.txt"),
+			expectedPath:  filepath.Join(expectedDir, "full_copy.txt"),
+			offset:        0,
+			limit:         0,
+			expectedError: nil,
+		},
+		{
+			name:          "file copy with offset",
+			fromPath:      filepath.Join(testDir, "source.txt"),
+			toPath:        filepath.Join(outputDir, "offset_copy.txt"),
+			expectedPath:  filepath.Join(expectedDir, "offset_copy.txt"),
+			offset:        12,
+			limit:         0,
+			expectedError: nil,
+		},
+		{
+			name:          "file copy with limit",
+			fromPath:      filepath.Join(testDir, "source.txt"),
+			toPath:        filepath.Join(outputDir, "limit_copy.txt"),
+			expectedPath:  filepath.Join(expectedDir, "limit_copy.txt"),
+			offset:        0,
+			limit:         10,
+			expectedError: nil,
+		},
+		{
+			name:          "file not found",
+			fromPath:      filepath.Join(testDir, "not_found.txt"),
+			toPath:        filepath.Join(outputDir, "error_copy.txt"),
+			expectedPath:  "",
+			offset:        0,
+			limit:         0,
+			expectedError: ErrUnsupportedFile,
+		},
+		{
+			name:          "file copy with offset exceeds file size",
+			fromPath:      filepath.Join(testDir, "source.txt"),
+			toPath:        filepath.Join(outputDir, "offset_exceeds_copy.txt"),
+			expectedPath:  "",
+			offset:        1000,
+			limit:         0,
+			expectedError: ErrOffsetExceedsFileSize,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Copy(tt.fromPath, tt.toPath, tt.offset, tt.limit)
+
+			if tt.expectedError != nil {
+				assert.ErrorIs(t, err, tt.expectedError, "should return error: %v, but got: %v", tt.expectedError, err)
+			} else {
+				assert.NoError(t, err, "shouldn`t return error, but got: %v", err)
+
+				expectedFileContent, err := os.ReadFile(tt.expectedPath)
+				require.NoError(t, err, "unexpected error reading expected file: %v", tt.expectedPath)
+				gotFileContent, err := os.ReadFile(tt.toPath)
+				require.NoError(t, err, "unexpected error reading got file: %v", tt.toPath)
+
+				assert.Equal(t, expectedFileContent, gotFileContent,
+					"content copied file should be: '%s',got: '%s'", string(expectedFileContent), string(gotFileContent))
+			}
+		})
+	}
 }
