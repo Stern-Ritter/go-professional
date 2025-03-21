4efbcede9bcac15365d3ada29c49b411f49c527a
Stern_Ritter <fadeef.k@yandex.ru>
2025-03-21 20:39:48 +0300

    feat: add lru cache

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -6,8 +6,14 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type entry struct {
+	Key   string
+	Value int
+}
 
 func TestCache(t *testing.T) {
 	t.Run("empty cache", func(t *testing.T) {
@@ -49,14 +55,78 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("pushing items out of cache due to cache capacity", func(t *testing.T) {
+		capacity := 5
+		c := NewCache(capacity)
+
+		entries := []entry{
+			{"a", 10},
+			{"b", 20},
+			{"c", 30},
+			{"d", 40},
+			{"e", 50},
+			{"f", 60},
+			{"g", 70},
+		}
+
+		for _, e := range entries {
+			inCache := c.Set(Key(e.Key), e.Value)
+			assert.False(t, inCache, "value for key: %s should not be in cache", e.Key)
+		}
+
+		pushedOutEntriesCount := len(entries) - capacity
+		pushedOutEntries := entries[:pushedOutEntriesCount]
+		for _, e := range pushedOutEntries {
+			v, inCache := c.Get(Key(e.Key))
+			assert.False(t, inCache, "value for key: %s should be pushed out from cache", e.Key)
+			assert.Empty(t, v, "value for key: %s should be empty but got: %d", e.Key, v)
+		}
+
+		expectedEntries := entries[pushedOutEntriesCount:]
+		for _, e := range expectedEntries {
+			v, inCache := c.Get(Key(e.Key))
+			assert.True(t, inCache, "value for key: %s should be in cache", e.Key)
+			assert.Equal(t, e.Value, v, "value for key: %s should be %d but got: %d", e.Key, e.Value, v)
+		}
+	})
+
+	t.Run("pushing last used items out of cache", func(t *testing.T) {
+		capacity := 3
+		c := NewCache(capacity)
+
+		entry1 := entry{"a", 10}
+		entry2 := entry{"b", 20}
+		entry3 := entry{"c", 30}
+		entries := []entry{entry1, entry2, entry3}
+
+		entry4 := entry{"d", 40}
+
+		for _, e := range entries {
+			inCache := c.Set(Key(e.Key), e.Value)
+			assert.False(t, inCache, "value for key: %s should not be in cache", e.Key)
+		}
+
+		c.Get(Key(entry1.Key))
+		c.Set(Key(entry2.Key), entry2.Value)
+		c.Set(Key(entry4.Key), entry4.Value)
+
+		pushedOutEntries := []entry{entry3}
+		for _, e := range pushedOutEntries {
+			v, inCache := c.Get(Key(e.Key))
+			assert.False(t, inCache, "value for key: %s should be pushed out from cache", e.Key)
+			assert.Empty(t, v, "value for key: %s should be empty but got: %d", e.Key, v)
+		}
+
+		expectedEntries := []entry{entry1, entry2, entry4}
+		for _, e := range expectedEntries {
+			v, inCache := c.Get(Key(e.Key))
+			assert.True(t, inCache, "value for key: %s should be in cache", e.Key)
+			assert.Equal(t, e.Value, v, "value for key: %s should be %d but got: %d", e.Key, e.Value, v)
+		}
 	})
 }
 
-func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
+func TestCacheMultithreading(_ *testing.T) {
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/Stern-Ritter/go-professional/hw04_lru_cache
 
 go 1.22
 
