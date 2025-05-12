fef9564d568d40060a31d557da03a31e889b0836
Stern_Ritter <fadeef.k@yandex.ru>
2025-05-13 00:37:41 +0300

    feat: add tasks worker pool

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,78 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if len(tasks) == 0 {
+		return nil
+	}
+
+	tasksCh := make(chan Task)
+	errsCh := make(chan error)
+	doneCh := make(chan struct{})
+	var wg sync.WaitGroup
+	errs := make([]error, 0, m)
+
+	go addTasks(tasksCh, doneCh, tasks)
+	startWorkerPool(tasksCh, errsCh, doneCh, &wg, n)
+
+	useErrorsLimit := m > 0
+	go func() {
+		for err := range errsCh {
+			errs = append(errs, err)
+			if useErrorsLimit && len(errs) >= m {
+				close(doneCh)
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(errsCh)
+
+	if useErrorsLimit && len(errs) >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
+}
+
+func addTasks(tasksCh chan<- Task, doneCh <-chan struct{}, tasks []Task) {
+	defer close(tasksCh)
+
+	for _, task := range tasks {
+		select {
+		case tasksCh <- task:
+		case <-doneCh:
+			return
+		}
+	}
+}
+
+func startWorkerPool(tasksCh <-chan Task, errsCh chan<- error, doneCh <-chan struct{}, wg *sync.WaitGroup,
+	workerCount int,
+) {
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go worker(tasksCh, errsCh, doneCh, wg)
+	}
+}
+
+func worker(tasksCh <-chan Task, errsCh chan<- error, doneCh <-chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case task, ok := <-tasksCh:
+			if !ok {
+				return
+			}
+			if err := task(); err != nil {
+				select {
+				case errsCh <- err:
+				case <-doneCh:
+					return
+				}
+			}
+		case <-doneCh:
+			return
+		}
+	}
 }
