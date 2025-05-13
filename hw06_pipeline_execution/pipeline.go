be49a217f9140d481c7f5241641b5cec73043f42
Stern_Ritter <fadeef.k@yandex.ru>
2025-05-13 20:21:39 +0300

    feat: add pipeline executor

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,39 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	ch := in
+
+	for _, stage := range stages {
+		ch = stageWrapper(ch, done, stage)
+	}
+
+	return ch
+}
+
+func stageWrapper(in In, done In, stage Stage) Out {
+	inner := make(Bi)
+	ch := stage(inner)
+
+	go func() {
+		defer close(inner)
+
+		for {
+			select {
+			case el, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case inner <- el:
+				}
+			case <-done:
+				<-in
+				return
+			}
+		}
+	}()
+
+	return ch
 }
