e7bfdf817a12a31552339fdc7e4c0d1fcc38fbbd
Stern_Ritter <fadeef.k@yandex.ru>
2025-05-17 19:59:17 +0300

    feat: add struct validator

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,7 +3,10 @@ package hw09structvalidator
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type UserRole string
@@ -42,10 +45,59 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Age:    20,
+				Email:  "test@example.com",
+				Role:   "admin",
+				Phones: []string{"12345678901"},
+			},
+			expectedErr: ValidationErrors{},
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "12345678",
+				Age:    17,
+				Email:  "invalid-email",
+				Role:   "user",
+				Phones: []string{"12345"},
+			},
+			expectedErr: ValidationErrors{
+				{Field: "ID", Err: fmt.Errorf("invalid string length: 12345678, should be: 36 but got: 8")},
+				{Field: "Age", Err: fmt.Errorf("invalid value: 17, should be greater or equal: 18")},
+				{Field: "Email", Err: fmt.Errorf("invalid string value: invalid-email, should match regexp: ^\\w+@\\w+\\.\\w+$")},
+				{Field: "Role", Err: fmt.Errorf("invalid value: user, should be in range: [admin stuff]")},
+				{Field: "Phones", Err: fmt.Errorf("invalid string length: 12345, should be: 11 but got: 5")},
+			},
+		},
+		{
+			in: App{
+				Version: "12345",
+			},
+			expectedErr: ValidationErrors{},
+		},
+		{
+			in: App{
+				Version: "1234",
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Version", Err: fmt.Errorf("invalid string length: 1234, should be: 5 but got: 4")},
+			},
+		},
+		{
+			in: Response{
+				Code: 200,
+			},
+			expectedErr: ValidationErrors{},
+		},
+		{
+			in: Response{
+				Code: 403,
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Code", Err: fmt.Errorf("invalid value: 403, should be in range: [200 404 500]")},
+			},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +105,12 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if tt.expectedErr == nil {
+				assert.NoError(t, err, "unexpected error: %s", err)
+			} else {
+				assert.True(t, reflect.DeepEqual(tt.expectedErr, err), "expected error: %v, but got: %v", tt.expectedErr, err)
+			}
 		})
 	}
 }
